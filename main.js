ce7fbc294fcca615ec66f4cd31baac65442f43dd
undying41 <undying41@users.noreply.github.com>
2024-02-26 13:19:09 +0100

    Work in progres

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 async function start() {
   const weatherPromise = await fetch(
-    'https://api.weather.gov/gridpoints/MFL/110,50/forecast'
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
   );
   const weatherData = await weatherPromise.json();
   const ourTemperature = weatherData.properties.periods[0].temperature;
-  document.querySelector('#temperature-output').textContent = ourTemperature;
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    console.log(pet.name);
+  });
+}
+
+petsArea();
