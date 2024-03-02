70bb0aaff7e26fb830a3dd76c425628fb1e42a10
undying41 <undying41@users.noreply.github.com>
2024-03-02 15:49:05 +0100

    Move to dynamic site

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ start();
 
 async function petsArea() {
   const petsPromise = await fetch(
-    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+    "https://teal-nasturtium-204088.netlify.app/.netlify/functions/pets"
   );
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
